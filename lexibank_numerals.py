3721a54affa692bc91c547f6e0b0fc170e9f88cf
chrzyki <christoph@functor-argument.net>
2019-09-23 11:47:33 +0200

    Download and split channumerals with download command.

--- lexibank_numerals.py
+++ lexibank_numerals.py
@@ -1,5 +1,17 @@
+import csv
+import pathlib
+from itertools import groupby
+
 from clldutils.path import Path
+from pycldf import Wordlist
+from pyglottolog import Glottolog
 from pylexibank.dataset import Dataset as BaseDataset
+
+# FIXME: Point to Zenodo or GitHub API?
+URL = "http://localhost:8000/cldf.zip"
+
+# FIXME: Remove absolute path.
+GL = "/home/rzymski@shh.mpg.de/Repositories/glottolog/glottolog"
 
 
 class Dataset(BaseDataset):
@@ -7,24 +19,46 @@ class Dataset(BaseDataset):
     id = "numerals"
 
     def cmd_download(self, **kw):
-        """
-        Download files to the raw/ directory. You can use helpers methods of `self.raw`, e.g.
+        channnumerals_files = [
+            "cldf-metadata.json",
+            "cognates.csv",
+            "forms.csv",
+            "languages.csv",
+            "parameters.csv",
+        ]
 
-        >>> self.raw.download(url, fname)
-        """
-        pass
+        glottolog = Glottolog(GL)
+        languoids = {l.id: l for l in glottolog.languoids()}
+
+        self.raw.download_and_unpack(
+            URL, *[Path("cldf", f) for f in channnumerals_files], log=self.log
+        )
+
+        channumerals = Wordlist.from_metadata("raw/cldf-metadata.json")
+        channumerals_forms = list(channumerals["FormTable"])
+
+        split_ft = [
+            list(f2)
+            for f1, f2 in groupby(
+                sorted(channumerals_forms, key=lambda f1: (f1["Language_ID"])),
+                lambda f1: (f1["Language_ID"]),
+            )
+        ]
+
+        for entry in split_ft:
+            lid = entry[0]["Language_ID"]
+            mc = "None"
+
+            if languoids[lid].macroareas:
+                mc = languoids[lid].macroareas[0].name
+
+            pathlib.Path("raw/" + mc).mkdir(parents=True, exist_ok=True)
+
+            with open("raw/" + mc + "/" + lid, "w") as outfile:
+                fp = csv.DictWriter(outfile, entry[0].keys())
+                fp.writeheader()
+                fp.writerows(sorted(entry, key=lambda x: int(x["Parameter_ID"])))
 
     def cmd_install(self, **kw):
-        """
-        Convert the raw data to a CLDF dataset.
-
-        Use the methods of `pylexibank.cldf.Dataset` after instantiating one as context:
-
-        >>> with self.cldf as ds:
-        ...     ds.add_sources(...)
-        ...     ds.add_language(...)
-        ...     ds.add_concept(...)
-        ...     ds.add_lexemes(...)
-        """
-        with self.cldf as ds:
+        with self.cldf as _:
             pass
--- raw/README.md
+++ raw/README.md
@@ -1,4 +1,1 @@
-# Source directory
-
-This directory contains the "raw" source data of the dataset from which the
-CLDF dataset in `cldf/` is derived.
+Raw data downloaded 2019-09-23T09:45:03.950478
