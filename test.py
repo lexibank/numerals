4fee616701bf4db599fc5c47bb9fd922967f4c56
chrzyki <christoph@functor-argument.net>
2019-10-02 14:29:38 +0200

    Added some utility function tests with the upstream data.

--- test.py
+++ test.py
@@ -1,2 +1,52 @@
+from pycldf import Wordlist
+
+from lexibank_numerals import CHANURL
+from numerals_util import split_form_table
+
+channumerals = split_form_table(Wordlist.from_metadata("tests/cldf-metadata.json"))
+
+
 def test_valid(cldf_dataset, cldf_logger):
     assert cldf_dataset.validate(log=cldf_logger)
+
+
+class TestUtil:
+    @staticmethod
+    def test_split_forms():
+        """
+        This is likely going to break with changes to the upstream data. However, I'd rather see
+        this fail in the tests first.
+        """
+        assert len(channumerals) == 5284
+        assert channumerals[0][0]["ID"] == "aari1239-1-1-1"
+        assert channumerals[0][0]["Language_ID"] == "aari1239-1"
+        assert channumerals[-1][0]["ID"] == "zuoj1238-1-1-1"
+
+    @staticmethod
+    def test_index_link():
+        from numerals_util import make_index_link
+
+        assert "* []()" == make_index_link("")
+        assert "* [Path/to/file](Path/to/file)" == make_index_link("Path/to/file")
+        assert "* [Spaces Spaces](Spaces%20Spaces)" == make_index_link("Spaces Spaces")
+
+    @staticmethod
+    def test_chan_link():
+        from numerals_util import make_chan_link
+
+        assert f" ([Source]({CHANURL}))" == make_chan_link("", CHANURL)
+        assert f" ([Source]({CHANURL}Bateri.htm))" == make_chan_link("Bateri.htm", CHANURL)
+
+    @staticmethod
+    def test_check_problems():
+        from numerals_util import check_for_problems
+
+        assert "" == check_for_problems(channumerals[0])
+        assert " **(Problems)**" == check_for_problems(channumerals[3])
+
+    @staticmethod
+    def test_language_name():
+        from numerals_util import make_language_name
+
+        assert "" == make_language_name()
+        assert " (Sindarin)" == make_language_name("Sindarin")
