f7f7df47bdfe5296e49c086051332346934afa42
Hans-Jörg Bibiko <bibiko@shh.mpg.de>
2020-02-26 17:24:33 +0100

    converted to cldfbench (download and makecldf); create cldf without recognizing overwrites

--- lexibank_numerals.py
+++ lexibank_numerals.py
@@ -1,10 +1,15 @@
 import csv
 import pathlib
+import attr
+import shutil
 
-from clldutils.path import Path
-from clldutils.path import walk
+from pyglottolog import Glottolog
+
+from clldutils.path import Path, walk
 from pycldf import Wordlist
 from pylexibank.dataset import Dataset as BaseDataset
+from pylexibank.models import Lexeme, Language
+from pylexibank.util import progressbar
 
 from numerals_util import (
     split_form_table,
@@ -18,34 +23,52 @@ from numerals_util import (
 CHANURL = "https://mpi-lingweb.shh.mpg.de/numeral/"
 
 # FIXME: Point to Zenodo or GitHub API?
-URL = "https://github.com/lexibank/channumerals/archive/v1.0.zip"
+URL = "https://github.com/lexibank/channumerals/raw/v1.0/cldf"
+
+
+@attr.s
+class NumeralsLanguage(Language):
+    SourceFile = attr.ib(default=None)
+    Contributor = attr.ib(default=None)
+    Base = attr.ib(default=None)
+    Comment = attr.ib(default=None)
+
+
+@attr.s
+class NumeralsLexeme(Lexeme):
+    Problematic = attr.ib(default=False)
+    Other_Form = attr.ib(default=None)
+    Variant_ID = attr.ib(default=1)
 
 
 class Dataset(BaseDataset):
     dir = Path(__file__).parent
     id = "numerals"
 
-    def cmd_download(self, **kw):
-        index = Path(self.raw / "index.md")
+    lexeme_class = NumeralsLexeme
+    language_class = NumeralsLanguage
+
+    channumerals_files = [
+        "cldf-metadata.json",
+        "cognates.csv",
+        "forms.csv",
+        "languages.csv",
+        "parameters.csv",
+        "sources.bib",
+    ]
+
+    def cmd_download(self, args):
+        glottolog = Glottolog('../glottolog')
+        index = Path(self.raw_dir / "index.md")
 
         # Create index always from scratch:
         if index.exists():
             Path.unlink(index)
 
-        channnumerals_files = [
-            "cldf-metadata.json",
-            "cognates.csv",
-            "forms.csv",
-            "languages.csv",
-            "parameters.csv",
-        ]
-
-        glottolog = self.glottolog
         languoids = {l.id: l for l in glottolog.languoids()}
 
-        self.raw.download_and_unpack(
-            URL, *[Path("channumerals-1.0/cldf", f) for f in channnumerals_files], log=self.log
-        )
+        for f in self.channumerals_files:
+            self.raw_dir.download("{0}/{1}".format(URL, f), f, log=args.log)
 
         channumerals = Wordlist.from_metadata("raw/cldf-metadata.json")
         split_ft = split_form_table(channumerals)
@@ -74,10 +97,10 @@ class Dataset(BaseDataset):
             except KeyError:
                 pass
 
-            pathlib.Path(self.raw / family).mkdir(parents=True, exist_ok=True)
+            pathlib.Path(self.raw_dir / family).mkdir(parents=True, exist_ok=True)
 
             # Write data from form table into respective CSV file:
-            with open(self.raw / family / csv_name, "w") as outfile:
+            with open(self.raw_dir / family / csv_name, "w") as outfile:
                 fp = csv.DictWriter(outfile, entry[0].keys())
                 fp.writeheader()
                 fp.writerows(sorted(entry, key=lambda x: int(x["Parameter_ID"])))
@@ -90,21 +113,36 @@ class Dataset(BaseDataset):
                 language_name = make_language_name(lt["Name"])
                 outfile.write(index_link + chan_link + language_name + problems + "\n")
 
-        # Cleanup:
-        for f in channnumerals_files:
-            Path.unlink(self.raw / f)
+        shutil.move(self.raw_dir / "cognates.csv", self.etc_dir / "cognates.csv")
+        shutil.move(self.raw_dir / "parameters.csv", self.etc_dir / "concepts.csv")
+        shutil.move(self.raw_dir / "languages.csv", self.etc_dir / "languages.csv")
 
-    def cmd_install(self, **kw):
-        with self.cldf as ds:
-            for c in walk(self.raw, mode="files"):
-                if c.name == "index.md" or c.name == "README.md":
-                    continue
-                with Path.open(c) as csvfile:
-                    reader = csv.DictReader(csvfile)
-                    for row in reader:
-                        ds.add_language(ID=row["Language_ID"], Glottocode=row["Language_ID"])
-                        ds.add_lexemes(
-                            Value=row["Value"],
-                            Language_ID=row["Language_ID"],
-                            Parameter_ID=row["Parameter_ID"],
-                        )
+    def cmd_makecldf(self, args):
+
+        args.writer.add_sources()
+
+        for concept in self.concepts:
+            args.writer.add_concept(**concept)
+        for language in self.languages:
+            args.writer.add_language(**language)
+
+        args.writer.cldf['FormTable', 'Problematic'].datatype.base = 'boolean'
+
+        for c in progressbar(sorted(walk(self.raw_dir, mode="files"), key=lambda k: k.name), desc="makecldf"):
+            if c.name == "index.md" or c.name == "README.md"\
+                    or c.name in self.channumerals_files\
+                    or c.name.startswith("."):
+                continue
+            with Path.open(c) as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    args.writer.add_lexemes(
+                        Value=row["Value"],
+                        Language_ID=row["Language_ID"],
+                        Parameter_ID=row["Parameter_ID"],
+                        Source="chan2019",
+                        Comment=row["Comment"],
+                        Other_Form=row["Other_Form"],
+                        Loan=bool(row["Loan"] == "True"),
+                        Variant_ID=row["Variant_ID"],
+                    )
