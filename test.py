baca664f30090fd8ffbd139d8b291a58ad30ce62
Hans-Jörg Bibiko <bibiko@shh.mpg.de>
2020-04-01 10:43:40 +0200

    added two cldf data tests

--- test.py
+++ test.py
@@ -10,6 +10,14 @@ def test_valid(cldf_dataset, cldf_logger):
     assert cldf_dataset.validate(log=cldf_logger)
 
 
+def test_languages(cldf_dataset):
+    assert any(l['SourceFile'] == 'Abua.htm' for l in cldf_dataset["LanguageTable"])
+
+
+def test_forms(cldf_dataset):
+    assert any(f["Other_Form"] == "een" for f in cldf_dataset["FormTable"])
+
+
 class TestUtil:
     @staticmethod
     def test_split_forms():
